81fb674e551fb675c5138405a97135c0ca936ca3
justin.bogan <jbogan@mullenlowe.com>
2016-08-03 12:28:46 -0400

    updated jquery version and moved some js around

---
+++ js/sitewide.js
@@ -0,0 +1,8 @@
+// 'use strict'; will help you write better code by being less lenient with syntax. Put the as the first line in all your js files.
+'use strict';
+      
+// On DOM ready, create a new App. No need to store this to a var. Its more secure this way.         
+$(function(){
+	new App();
+	console.log('sitewide');
+});
